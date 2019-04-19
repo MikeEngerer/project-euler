b86fa7701e633b6d5eeeabc952e641edd3cfc3ff
MikeEngerer <michael.engerer@hotmail.com>
2019-04-19 18:11:34 -0400

    sol to 4

---
+++ solutions/4.js
@@ -0,0 +1,24 @@
+const { runtime } = require('../runtime.js')
+
+const isPalindrome = (num) => {
+  let strNum = num.toString()
+  let reverseNum = strNum.split('').reverse().join('')
+  return strNum === reverseNum
+}
+
+const numProductPalidrome = () => {
+  let prod;
+  let largestPalindrome = 0
+  for (let i = 999; i > 99; i--) {
+    for (let j = 999; j > 99; j--) {
+      prod = i * j
+      if (isPalindrome(prod) && prod > largestPalindrome) {
+        largestPalindrome = prod
+      }
+      prod = null;
+    }
+  }
+  return largestPalindrome
+}
+
+runtime(numProductPalidrome)
