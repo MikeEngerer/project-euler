9e3f0c73c60fcedf4d53a3c000e95339f88cd466
MikeEngerer <michael.engerer@hotmail.com>
2019-04-20 13:37:21 -0400

    isprime module includes func to gen prime arr

--- isPrime.js
+++ isPrime.js
@@ -7,6 +7,26 @@ const isPrime = (num) => {
   return true
 }
 
+const genPrimeArr = (numPrimes) => {
+  let primes = [2,3,5,7,11]
+  let newPrime = 13
+  while (primes.length < numPrimes) {
+    let len = primes.length
+    for (let i = 0; i < len; i++) {
+      if (newPrime % primes[i] === 0) {
+        break
+      }
+      if (i === (len - 1) && newPrime % primes[i] !== 0) {
+        primes.push(newPrime)
+      }
+    }
+    newPrime += 2
+  }
+  return primes
+}
+
+
 module.exports = {
-  isPrime
+  isPrime,
+  genPrimeArr
 }
