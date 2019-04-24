36e0c51c22a31b2f50b61bb884c4effba20acefd
MikeEngerer <michael.engerer@hotmail.com>
2019-04-23 22:21:52 -0400

    sol to 13

---
+++ solutions/13.js
@@ -0,0 +1,19 @@
+const { data } = require('../project_euler/13')
+const { runtime } = require('../runtime')
+ 
+// format nums into arr
+const dataArr = data.split('\n').map(e => e.trim())
+
+/*
+Breakdown of shorthand used below:
+    -sum arr of nums with reduce -> returns sum in sci notation
+    -convert to str
+    -remove unneeded chars
+        -split on decimal, join back together
+        -split on e, join back together
+        -slice the first 10 chars, join
+    -returns first 10 digits as a str
+*/
+const firstTenDigits = (data) => data.reduce((a, e) => a + parseInt(e), 0).toString().split('.').join('').split('').slice(0, 10).join('')
+
+runtime(firstTenDigits, dataArr)
