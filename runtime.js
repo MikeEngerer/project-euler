e93e9bf91bca5ce8e840146eb873fbc717e96c86
MikeEngerer <michael.engerer@hotmail.com>
2019-04-23 23:48:34 -0400

    refactor runtime module to handle multiple arguments

--- runtime.js
+++ runtime.js
@@ -1,8 +1,11 @@
-const runtime = (func, args) => {
+const runtime = function (func, args) {
+  let argArr = Array.prototype.slice.call(arguments, 1)
   let currentTime = Date.now()
-  console.log('func returned:', func(args))
+  console.log('func returned', func(...argArr))
   console.log('seconds elapsed:', (Date.now() - currentTime) / 1000)
+  return
 }
+
 
 module.exports = {
   runtime
