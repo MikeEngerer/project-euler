5c03648d7067711b60a12b554e383ca5d24c14c6
MikeEngerer <michael.engerer@hotmail.com>
2019-04-26 17:16:00 -0400

    sol to 67 (same as 18)

---
+++ solutions/67.js
@@ -0,0 +1,26 @@
+const { runtime } = require('../runtime')
+const path = require('path')
+const fs = require('fs')
+
+let data = fs.readFileSync(path.join(__dirname, '../project_euler/triangle.txt'), 'utf-8')
+
+data = data.split('\n').map(e => e.trim()).map(e => e.split(' ').map(el => Number(el)))
+// ? extra ele [0]...
+data.pop()
+// same sol as 18
+const findMax = (data) => {
+  let current, left, right
+  for (let i = data.length - 1; i > 0; i--) {
+    for (let j = 0; j < data[i - 1].length; j++) {
+      current = data[i - 1][j]
+      left = data[i][j]
+      right = data[i][j + 1]
+      left > right ? current += left : current += right
+      data[i - 1][j] = current
+    }
+  }
+  // starting num is now max route summed
+  return data[0][0]
+}
+
+runtime(findMax, data)
