7671f8885a49f1bf5785a953265846fccf337c7c
MikeEngerer <michael.engerer@hotmail.com>
2019-04-27 17:34:12 -0400

    sol to 22 and alpha module

---
+++ solutions/22.js
@@ -0,0 +1,14 @@
+const { runtime } = require('../runtime')
+const { genAlphaObj } = require('../alphabet')
+const path = require('path')
+const fs = require('fs')
+// import and format data into arr, then sort alphabetically
+let data = fs.readFileSync(path.join(__dirname, '../project_euler/names.txt'), 'utf-8').split('"').join('').split(',').sort()
+// generate obj containing letter scores
+const alphaScores = genAlphaObj('A', 'Z')
+// returns sum of one name
+const getNameSum = (name) => name.split('').reduce((a, e) => a += alphaScores[e], 0)
+// reduce and return total sum
+const findTotal = (data) => data.reduce((a, e, i) => a += (getNameSum(e) * (i + 1)), 0)
+
+runtime(findTotal, data)
