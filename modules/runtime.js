4f4cff2ca60928374d11f56bafe1cdae06afab2d
MikeEngerer <michael.engerer@hotmail.com>
2019-04-29 15:45:17 -0400

    update readme

--- modules/runtime.js
+++ modules/runtime.js
@@ -1,6 +1,6 @@
 const runtime = function (func, args) {
   let argArr = Array.prototype.slice.call(arguments, 1), initialTime = Date.now()
-  console.log('func returned', func(...argArr))
+  console.log('func returned:', func(...argArr))
   console.log('runtime:', `${(Date.now() - initialTime) / 1000}s`)
   return
 }
