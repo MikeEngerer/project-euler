bc83b1fc4e7c8d5703a05f2c8e6b1e8aae591a8c
MikeEngerer <michael.engerer@hotmail.com>
2019-04-28 22:29:26 -0400

    var rename

--- modules/runtime.js
+++ modules/runtime.js
@@ -1,8 +1,7 @@
 const runtime = function (func, args) {
-  let argArr = Array.prototype.slice.call(arguments, 1)
-  let currentTime = Date.now()
+  let argArr = Array.prototype.slice.call(arguments, 1), initialTime = Date.now()
   console.log('func returned', func(...argArr))
-  console.log('runtime:', `${(Date.now() - currentTime) / 1000}s`)
+  console.log('runtime:', `${(Date.now() - initialTime) / 1000}s`)
   return
 }
 
