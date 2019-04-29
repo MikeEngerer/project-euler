409be91116f40996d2fde324ca623d376df32739
MikeEngerer <michael.engerer@hotmail.com>
2019-04-29 19:43:19 -0400

    py sol to 20

---
+++ solutions/20.py
@@ -0,0 +1,14 @@
+# fed up with JS lack of big num support, will come back and do this in JS eventually
+def factorial(num):
+  if (num == 0):
+    return 1
+  else:
+    return num * factorial(num - 1)
+
+def sum(num):
+  sum = 0
+  for digit in str(num):
+    sum = sum + int(digit)
+  return sum
+
+print(sum(factorial(100)))
