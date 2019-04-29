635d47fe99115264118105e505f453d985fe651e
MikeEngerer <michael.engerer@hotmail.com>
2019-04-28 22:20:40 -0400

    update return statement

--- modules/runtime.js
+++ modules/runtime.js
@@ -2,7 +2,7 @@ const runtime = function (func, args) {
   let argArr = Array.prototype.slice.call(arguments, 1)
   let currentTime = Date.now()
   console.log('func returned', func(...argArr))
-  console.log('seconds elapsed:', (Date.now() - currentTime) / 1000, 's')
+  console.log('runtime:', `${(Date.now() - currentTime) / 1000}s`)
   return
 }
 
