163dafcde78c708ec745b4273183a6a32d0dda3b
MikeEngerer <michael.engerer@hotmail.com>
2019-05-11 17:23:22 -0400

    sol to 41

---
+++ solutions/41.js
@@ -0,0 +1,19 @@
+const { runtime, isPandigital, isPrime } = require('project-euler-helpers') 
+
+// check if pandigital first, then if prime (2x faster than reverse order)
+const findPandigitalPrimes = (max) => {
+  let numDigits, highest = 0
+  for (let i = 11; i < max; i += 2) {
+    numDigits = i.toString().length
+    if (isPandigital(i, numDigits)) {
+      if (isPrime(i)) {
+        highest = i
+      }
+    }
+  }
+  return highest
+}
+
+// upper bound is intuitively 1000000, sped up slightly by lowering to 8000000 experimentally
+runtime(findPandigitalPrimes, 8000000)
+// runtime: 3.157s
